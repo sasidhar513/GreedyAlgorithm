9189cf891dc9e2f1e6218c65c208049c5cb76cb2
sasidhar513 <sasidhar.dulipudi@tcs.com>
2017-01-23 18:04:01 +0530

    updated KrushkalsMinimumSpanningTreees

--- GreedyAlgorithm.cpp
+++ GreedyAlgorithm.cpp
@@ -74,7 +74,7 @@ double KnapsackFractionGreedy(Item item[],int size,int maxWeight)
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
-* Activity selection problem(Two arrays are given one for start time and other for end time you need to select maximum no of tasks he can complete)
+/* Activity selection problem(Two arrays are given one for start time and other for end time you need to select maximum no of tasks he can complete)
             logic: The end time arrow is  sorted in ascending order so
                 Pick the minimum end time and do that job. 
                 pick the next min end time if the start time is after the previous end time else go for next min.
@@ -99,12 +99,47 @@ void ActivitySelection(int start[],int end[],int size)
     }
     return;
 }
-
 //-----------------------------------------------------------------------------------------------------------------------------------------
-* KrushkalsMinimumSpanningTreees
-
-
-
+/* KrushkalsMinimumSpanningTreees (Given the undirected weighed graph,the Minimum Weight Spanning Tree is the sub graph that is a tree that connects all nodes and total weight is minimum 
+            logic: save graph 
+                   sort the Edges based on weight in the increasing order
+                   loop while the result contains V-1 edges:
+                        take each edge and form a tree using union-find(that checks if there are any loops )
+                        if that edge doesnt form a loop  add it to the result
+            Required Modules:
+                        Edge
+                        Graph
+                        addEdge
+                        CompareEdges
+                        subGraph
+                        find
+                        unionFind
+                        KrushkalMST
+            sample ecample to run the code. copy the following in main()
+                            int V = 4;  // Number of vertices in graph
+                            int E = 5;  // Number of edges in graph
+                            Graph* graph= addEdge(V,E);   
+                            // add edge 0-1
+                            graph->edges[0].source = 0;
+                            graph->edges[0].dest = 1;
+                            graph->edges[0].dist = 10;
+                            // add edge 0-2
+                            graph->edges[1].source = 0;
+                            graph->edges[1].dest = 2;
+                            graph->edges[1].dist = 6; 
+                            // add edge 0-3
+                            graph->edges[2].source = 0;
+                            graph->edges[2].dest = 3;
+                            graph->edges[2].dist = 5;
+                            // add edge 1-3
+                            graph->edges[3].source = 1;
+                            graph->edges[3].dest = 3;
+                            graph->edges[3].dist = 15; 
+                            // add edge 2-3
+                            graph->edges[4].source = 2;
+                            graph->edges[4].dest = 3;
+                            graph->edges[4].dist = 4;
+                            KrushkalMST(graph);
 */
 struct Edge
 {
@@ -115,10 +150,9 @@ struct Edge
 struct Graph     
 {
     int V, E;
-    Edge *edges;
-    
+    Edge *edges;    
 };
- Graph * addEdge(int v,int e)
+Graph * addEdge(int v,int e)
 {
     Graph * graph=(struct Graph*) malloc( sizeof(struct Graph) );;
     graph->V=v;
@@ -132,62 +166,56 @@ int CompareEdges(void const* a,void  const*b)
     Edge * b1=(Edge *) b;
     return a1->dist>b1->dist;
 }
-
 struct subGraph
 {
-    int v;
+    int parent;
     int rank;
-}
-int find(int child,subGraph *parent)
+};
+int find(int child,subGraph *parents)
 {
-    if()
+    if(parents[child].parent!=child)
+        parents[child].parent=find(parents[child].parent,parents);
+    return parents[child].parent;
 }
-void print(Graph * g)
+void unionFind(int child1,int child2,subGraph* parents)
 {
-    for(int i=0;i<g->E;i++)
+    int  parent1=find(child1,parents);
+    int parent2=find(child2,parents);
+  
+    if(parents[parent1].rank>parents[parent2].rank)
+        parents[parent2].parent=parent1;
+    else if(parents[parent2].rank>parents[parent1].rank)
+        parents[parent1].parent=parent2;
+    else 
     {
-        cout<<g->edges[i].source<<g->edges[i].dest<<endl;
+        parents[parent2].parent=parent1;
+        parents[parent1].rank+=1;    
     }
 }
-
-
-
-
-int main()
+void KrushkalMST(Graph  * graph)
 {
-
-     int V = 4;  // Number of vertices in graph
-    int E = 5;  // Number of edges in graph
-
-     Graph* graph= addEdge(V,E);
-
-    
-    // add edge 0-1
-    graph->edges[0].source = 0;
-    graph->edges[0].dest = 1;
-    graph->edges[0].dist = 10;
-               cout<<"dkfasldfa"<<endl;
-
-    // add edge 0-2
-    graph->edges[1].source = 0;
-    graph->edges[1].dest = 2;
-    graph->edges[1].dist = 6;
- 
-    // add edge 0-3
-    graph->edges[2].source = 0;
-    graph->edges[2].dest = 3;
-    graph->edges[2].dist = 5;
-
-    // add edge 1-3
-    graph->edges[3].source = 1;
-    graph->edges[3].dest = 3;
-    graph->edges[3].dist = 15;
- 
-    // add edge 2-3
-    graph->edges[4].source = 2;
-    graph->edges[4].dest = 3;
-    graph->edges[4].dist = 4;
-
     qsort(graph->edges,graph->E,sizeof(graph->edges[0]),CompareEdges);
-    print(graph);
+    subGraph *g=(subGraph *)malloc(graph->V*sizeof(subGraph));
+    for( int i=0;i<graph->V;i++)
+    {
+        g[i].parent=i;
+        g[i].rank=0;
+    }
+    Edge * result= (Edge  *)malloc((graph->V-1)*sizeof(Edge));
+    int j=0,i=0;
+    while(j<(graph->V-1))
+    {
+        int vertex1= find(graph->edges[i].source,g);
+        int vertex2= find(graph->edges[i].dest,g);
+        if(vertex1!=vertex2)
+        {
+            cout<<vertex1<<vertex2<<i<<endl;            
+             result[j++]=graph->edges[i];
+             unionFind(vertex1, vertex2, g);
+        }
+    i++;        
+    }
+    for(int i=0;i<j;i++)
+        cout<<result[i].source<<"\t"<<result[i].dest<<"     "<<result[i].dist<<endl;
 }
+//-------------------------------------------------------------------------------------------------------------------------------------
